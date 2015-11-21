041247515797d98aa71e0b89d247cc7dc8066dbd
Phillip Cloud <cpcloud@gmail.com>
2015-11-21 09:30:31 -0600

    Cleanup compute csv

--- blaze/compute/csv.py
+++ blaze/compute/csv.py
@@ -21,7 +21,7 @@ from ..expr.optimize import lean_projection
 from .pmap import get_default_pmap
 
 
-__all__ = ['optimize', 'pre_compute', 'compute_chunk', 'compute_down']
+__all__ = ['optimize', 'pre_compute']
 
 
 @dispatch(Expr, CSV)
@@ -54,7 +54,8 @@ def pre_compute(expr, data, comfortable_memory=None, chunksize=2**18, **kwargs):
         return into(pd.DataFrame, data, dshape=leaf.dshape, **kwargs)
 
 
-Cheap = (Head, ElemWise, Distinct, Symbol)
+Cheap = Head, ElemWise, Distinct, Symbol
+
 
 @dispatch(Head, CSV)
 def pre_compute(expr, data, **kwargs):
@@ -63,27 +64,3 @@ def pre_compute(expr, data, **kwargs):
         return into(Iterator, data, chunksize=10000, dshape=leaf.dshape)
     else:
         raise MDNotImplementedError()
-
-
-def compute_chunk(chunk, chunk_expr, part):
-    return compute(chunk_expr, {chunk: part})
-
-
-@dispatch(Expr, pandas.io.parsers.TextFileReader)
-def compute_down(expr, data, map=None, **kwargs):
-    if map is None:
-        map = get_default_pmap()
-    leaf = expr._leaves()[0]
-
-    (chunk, chunk_expr), (agg, agg_expr) = split(leaf, expr)
-
-    parts = list(map(curry(compute_chunk, chunk, chunk_expr), data))
-
-    if isinstance(parts[0], np.ndarray):
-        intermediate = np.concatenate(parts)
-    elif isinstance(parts[0], pd.DataFrame):
-        intermediate = pd.concat(parts)
-    elif isinstance(parts[0], (Iterable, Iterator)):
-        intermediate = concat(parts)
-
-    return compute(agg_expr, {agg: intermediate})
